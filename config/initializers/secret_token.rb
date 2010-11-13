c64829eb3e4689c3e219cdabf5fea34c12dca75a
thordurg08 <thordurg08@ru.is>
2010-11-13 00:02:26 +0000

    first commit part two

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Totaliceland::Application.config.secret_token = 'ce469117848181a5364a7863ce33aa501025fa5f3c378e35dc06869f09b26decf9fa338fcc603b90f433e66a2f64b7d4c58cf6c8ea263f3a1c5e786aabfb011a'
