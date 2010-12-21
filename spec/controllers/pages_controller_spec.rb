96c57ed709259564dc3ebc3e320fd1d13b036978
Thordur Gunnarsson <thordurg08@ru.is>
2010-12-21 04:52:15 -0800

    specs and app helper

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Total Iceland"
+  end
+
   # Home
   describe "GET 'home'" do
     it "should be successful" do
@@ -13,7 +17,7 @@ describe PagesController do
   it "should have the right title" do
     get 'home'
     response.should have_selector("title",
-	:content => "Total Iceland | Home")
+	:content => @base_title + " | Home")
     end
   end
 
@@ -27,7 +31,7 @@ describe PagesController do
     it "should have the right title" do
     get 'contact'
     response.should have_selector("title",
-	:content => "Total Iceland | Contact")
+	:content => @base_title + " | Contact")
     end
   end
 
@@ -41,7 +45,7 @@ describe PagesController do
     it "should have the right title" do
     get 'about'
     response.should have_selector("title",
-	:content => "Total Iceland | About")
+	:content => @base_title + " | About")
     end
   end
 end
